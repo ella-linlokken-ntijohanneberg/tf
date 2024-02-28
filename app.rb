32319dc16584f052e6603053b518c1e8109261ad
ellalinlokken-ntijoanneberg <118273700+ella-linlokken-ntijohanneberg@users.noreply.github.com>
2024-02-28 16:33:11 +0100

    28 feb

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
@@ -9,5 +10,17 @@ get('/') do
 end
 
 get('/projects') do
-    
+  db = SQLite3::Database.new('db/handmade.db')
+  db.results_as_hash = true
+  @result = db.execute("SELECT * FROM projects")
+  p @result
+  slim(:"projects/index")
+end
+
+get('/projects/:id') do
+  id = params[:id].to_i
+  db = SQLite3::Database.new('db/handmade.db')
+  db.results_as_hash = true
+  @result = db.execute("SELECT * FROM projects WHERE project_id = ?", id)
+  slim(:"projects/show")
 end
