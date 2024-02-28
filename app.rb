1a32d361146db0f164c8342711583cf11e42d434
ellalinlokken-ntijoanneberg <118273700+ella-linlokken-ntijohanneberg@users.noreply.github.com>
2024-02-28 22:19:22 +0100

    28 feb

--- app.rb
+++ app.rb
@@ -17,10 +17,30 @@ get('/projects') do
   slim(:"projects/index")
 end
 
+get('/projects/new') do
+  slim(:"projects/new")
+end
+
+post('/projects/new') do
+  name = params[:project_name]
+  craft_id = params[:craft_id].to_i
+  user_id = params[:user_id].to_i
+  db = SQLite3::Database.new('db/handmade.db')
+  db.execute("INSERT INTO projects (name, craft_type_id, user_id) VALUES (?,?,?)", name, craft_id, user_id)
+  redirect('/projects')
+end
+
+post('/projects/:id/delete') do
+  id = params[:id].to_i
+  db = SQLite3::Database.new('db/handmade.db')
+  db.execute("DELETE FROM projects WHERE project_id = ?", id)
+  redirect('/projects')
+end
+
 get('/projects/:id') do
   id = params[:id].to_i
   db = SQLite3::Database.new('db/handmade.db')
   db.results_as_hash = true
-  @result = db.execute("SELECT * FROM projects WHERE project_id = ?", id)
+  @result = db.execute("SELECT * FROM projects WHERE project_id = ?", id).first
   slim(:"projects/show")
 end
